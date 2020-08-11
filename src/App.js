fabf9d0aeda699241adb8c13c345da6a83807f40
Adam Mathieson <mathieson1222@gmail.com>
2020-08-11 01:08:41 -0700

    downloads deps and renders svg yogi to page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 
+import Yogi from './Yogi'
+
+
 
 
 function App() {
   return (
     <div className="App">
       <h1>Yoga</h1>
+      <Yogi />
     </div>
   );
 }
